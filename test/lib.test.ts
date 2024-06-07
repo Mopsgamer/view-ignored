3bc2dc1a8fccfae5b42294d1660b49b322fcc838
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2024-06-07 11:22:20 +0200

    improve tests

--- test/lib.test.ts
+++ test/lib.test.ts
@@ -5,7 +5,7 @@ import type FileSystem from "mock-fs/lib/filesystem.js"
 import { npmPatternExclude, npmPatternInclude } from "../src/index.js"
 
 interface Case {
-    expect: string[]
+    shouldInclude: string[]
     content: FileSystem.DirectoryItem
 }
 type DirCase = Record<string, Case>
@@ -14,17 +14,17 @@ type Plan = Record<viewig.TargetName, DirCase>
 const targetTestList = {
     git: {
         'empty project': {
-            expect: [],
+            shouldInclude: [],
             content: {},
         },
         'single file, no .gitignore': {
-            expect: ['file.txt'],
+            shouldInclude: ['file.txt'],
             content: {
                 'file.txt': ''
             },
         },
         'minimal project: .gitignore': {
-            expect: ['file.txt', '.gitignore'],
+            shouldInclude: ['file.txt', '.gitignore'],
             content: {
                 'file.txt': '',
                 'node_modules': {
@@ -41,17 +41,17 @@ const targetTestList = {
      */
     npm: {
         'empty project': {
-            expect: [],
+            shouldInclude: [],
             content: {},
         },
         'single file, no ignore sources': {
-            expect: ['file.txt'],
+            shouldInclude: ['file.txt'],
             content: {
                 'file.txt': ''
             },
         },
         'minimal project: .gitignore': {
-            expect: ['file.txt'],
+            shouldInclude: ['file.txt'],
             content: {
                 'file.txt': '',
                 'node_modules': {
@@ -63,7 +63,7 @@ const targetTestList = {
             },
         },
         'minimal project: .npmignore, .gitignore': {
-            expect: ['file2.txt'],
+            shouldInclude: ['file2.txt'],
             content: {
                 'file.txt': '',
                 'file2.txt': '',
@@ -76,35 +76,82 @@ const targetTestList = {
                 '.gitignore': 'file2.txt'
             },
         },
+        'minimal project: package.json, .npmignore, .gitignore': {
+            shouldInclude: ['file2.txt', 'package.json'],
+            content: {
+                'file.txt': '',
+                'file2.txt': '',
+                'node_modules': {
+                    'tempdep': {
+                        'indexOf.js': ''
+                    }
+                },
+                '.npmignore': 'file.txt',
+                '.gitignore': 'file2.txt',
+                'package.json': JSON.stringify({
+                    files: ['file2.txt']
+                })
+            },
+        },
+        'minimal project: .npmignore, .gitignore, "{}" package.json': {
+            shouldInclude: ['file2.txt', 'package.json'],
+            content: {
+                'file.txt': '',
+                'file2.txt': '',
+                'node_modules': {
+                    'tempdep': {
+                        'indexOf.js': ''
+                    }
+                },
+                '.npmignore': 'file.txt',
+                '.gitignore': 'file2.txt',
+                'package.json': '{}'
+            },
+        },
+        'minimal project: .npmignore, .gitignore, "" package.json': {
+            shouldInclude: ['file2.txt', 'package.json'],
+            content: {
+                'file.txt': '',
+                'file2.txt': '',
+                'node_modules': {
+                    'tempdep': {
+                        'indexOf.js': ''
+                    }
+                },
+                '.npmignore': 'file.txt',
+                '.gitignore': 'file2.txt',
+                'package.json': ''
+            },
+        },
     },
     yarn: {
         'empty project': {
-            expect: [],
+            shouldInclude: [],
             content: {},
         },
     },
     vscodeExtension: {
         'empty project': {
-            expect: [],
+            shouldInclude: [],
             content: {},
         },
     },
 } as const satisfies Plan
 
+const testPath = './test/.simulation'
 describe("Targets", function () {
     for (const target in targetTestList) {
         describe(target, function () {
             const tests = targetTestList[target] as DirCase
             for (const testName in tests) {
+                const test = tests[testName] as Case
                 it(testName, function () {
-                    const test = tests[testName] as Case
-                    const testPath = './test/simulation'
                     mockfs({ [testPath]: test.content })
-                    const lookList = viewig.lookProjectDirSync({ cwd: testPath, ...viewig.Presets[target] })
-                    assert.deepEqual(lookList.map(l => l.filePath).sort(), test.expect.sort())
-                    mockfs.restore()
+                    const lookList = viewig.lookProjectDirSync({ cwd: testPath, filter: 'included', ...viewig.Presets[target] })
+                    assert.deepEqual(lookList.map(l => l.filePath).sort(), test.shouldInclude.sort())
                 })
             }
         })
     }
 })
+mockfs.restore()
