1f479f740c7e9304472daf730dc7e380da614cc9
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2024-07-04 20:55:28 +0200

    add config jdocs

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,14 @@ import propertiesFile from "properties";
 import path from "path"
 import { existsSync, readFileSync, rmSync, writeFileSync } from "fs"
 
+/**
+ * The full config file name - `".view-ignored"`.
+ */
 export const configFileName = ".view-ignored"
+/**
+ * The user's home directory + the config file name.
+ * @see {@link os.homedir}
+ */
 export const configFilePath = path.join(os.homedir(), configFileName)
 
 export const configKeyList = ["color", "target", "filter", "sort", "style"] as const satisfies readonly (keyof Config)[]
@@ -25,6 +32,10 @@ export const configDefault: Config = {
     sort: "firstFolders",
     style: "treeEmoji",
 }
+
+/**
+ * Represents allowed values for each config property.
+ */
 export const configValues = {
     color: ["1", "2", "3", "4"],
     target: Binding.targetList(),
@@ -33,6 +44,10 @@ export const configValues = {
     style: Styling.styleNameList
 } as const
 
+/**
+ * @see {@link configValues}
+ * @returns `true` if the value is a {@link Config}.
+ */
 export function configPartialGood(cfg: unknown): cfg is Partial<Config> {
     if (cfg?.constructor !== Object) {
         return false
@@ -45,11 +60,19 @@ export function configPartialGood(cfg: unknown): cfg is Partial<Config> {
     )
 }
 
+/**
+ * The config manipulator.
+ */
 export const configManager = {
     /**
-     * Contains custom settings.
+     * Do not change this value directly.
+     * @see {@link configManager}.
      */
     data: {} as Partial<Config>,
+    /**
+     * Loads the config from the file to {@link configManager.data}. If the data is not valid, throws an error without loading.
+     * @returns `undefined` if the config file does not exist.
+     */
     load() {
         const parsed: unknown | undefined = existsSync(configFilePath) ? propertiesFile.parse(readFileSync(configFilePath).toString()) : undefined
         if (parsed === undefined) {
@@ -62,7 +85,7 @@ export const configManager = {
         return this
     },
     /**
-     * Saves config to the file. If there are no settings, the file will be deleted, if exists.
+     * Saves the partial config to the file. If there are no settings, the file will be deleted, if exists.
      */
     save() {
         if (Object.keys(this.data).length === 0) {
@@ -74,13 +97,20 @@ export const configManager = {
         writeFileSync(configFilePath, propertiesFile.stringify(this.data)!)
         return this
     },
+    /**
+     * Sets a new value for the specified config property.
+     * Expects a valid value.
+     * @param key The name of the config property.
+     * @param value The new value for the config property.
+     */
     set<T extends ConfigKey>(key: T, value: Config[T]) {
         this.data[key] = value
         return this
     },
     /**
-     * Deletes specific key or all keys from config.
-     * @param key Config property name.
+     * Deletes the specified property from the config.
+     * If the property is not specified, then all properties will be deleted.
+     * @param key The config property.
      */
     unset<T extends ConfigKey>(key?: T) {
         if (key === undefined) {
@@ -93,22 +123,22 @@ export const configManager = {
         return this
     },
     /**
-     * Returns specific property value.
-     * @param key Config property name.
-     * @param fallbackDefault If `true`, default config value will be used when property from config file is `undefined`. Default `true`.
+     * Returns the value for the specified property.
+     * @param key The config property.
+     * @param fallbackDefault If `true`, the default value will be used when the value is `undefined`. Default `true`.
      */
-    get<T extends ConfigKey, UseDefault extends boolean>(key: T, fallbackDefault?: UseDefault) {
-        const useDefault = fallbackDefault ?? true
+    get<T extends ConfigKey>(key: T, fallbackDefault: boolean = true): typeof fallbackDefault extends true ? Config[T] : Config[T] | undefined {
         const value: Config[T] | undefined = this.data[key]
-        if (useDefault && value === undefined) {
+        if (fallbackDefault && value === undefined) {
             return configDefault[key]
         }
         return value
     },
     /**
-     * If key specified, returns string in format `"key=value"`. Otherwise, returns string with all pairs, separated with '\n' in format `"key=value\nkey=value\n..."` without new line ending.
-     * @param key Config property name.
-     * @param fallbackDefault If `true`, default config value will be used when property from config file is `undefined`. Default `true`.
+     * @returns The string in the `"key=value"` format, if the property is specified.
+     * Otherwise in format `"key=value\nkey=value\n..."` without new line ending.
+     * @param key The config property.
+     * @param fallbackDefault If `true`, the default value will be used when the value is `undefined`. Default `true`.
      */
     getPairString<T extends ConfigKey>(key?: T, fallbackDefault: boolean = true): string {
         if (key === undefined) {
