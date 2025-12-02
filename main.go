d6f5f7d4c97f8d3afa28bb68143062278b9be695
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2025-12-02 14:05:29 +0100

    style: print func

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"slices"
-	"strings"
-	"time"
 
 	"github.com/Mopsgamer/view-ignored/internal"
 	"github.com/Mopsgamer/view-ignored/internal/targets"
@@ -37,31 +34,8 @@ func main() {
 			fmt.Printf("error: unsupported target: %s, supported targets are "+targets.SupportedTargetsList()+"\n", *target)
 			os.Exit(1)
 		}
-		mainScan(targets.Target(*target), *invert)
+		internal.Print(targets.Target(*target), *invert)
 	case "help", "":
 		flag.Usage()
-	}
-}
-
-func mainScan(target targets.Target, invert bool) {
-	fmt.Println("Target: " + target)
-	pwd, _ := os.Getwd()
-	fmt.Println("PWD: " + pwd)
-	fmt.Println("")
-
-	start := time.Now()
-	ctx := internal.Scan(target, internal.ScanOptions{
-		Invert: &invert,
-	})
-	slices.SortFunc(ctx.Paths, internal.FirstFolders)
-	fmt.Println(strings.Join(ctx.Paths, "\n"))
-	fmt.Printf("\nLooked through %d files and %d dirs", ctx.TotalFiles, ctx.TotalDirs)
-	fmt.Printf("\nMatched %d files in %v\n", len(ctx.Paths), time.Since(start))
-
-	if len(ctx.SourceErrors) > 0 {
-		fmt.Printf("\nFound %d errors\n", len(ctx.SourceErrors))
-		for _, err := range ctx.SourceErrors {
-			fmt.Println(err.Error())
-		}
 	}
 }
