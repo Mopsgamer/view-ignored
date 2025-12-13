5a862f0efcd3f1e1ed985495c2a6fd9cf3dba245
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2025-12-13 02:06:49 +0100

    almost

--- src/patterns/gitignore.ts
+++ src/patterns/gitignore.ts
@@ -1,4 +1,26 @@
+import type { Source } from './matcher.js'
 import { minimatch } from 'minimatch'
+
+export function extractGitignore(source: Source, content: Buffer<ArrayBuffer>): void {
+	for (let line of content.toString().split('\n')) {
+		line = line.trim()
+		if (line === '' || line.startsWith('#')) {
+			continue
+		}
+		const cdx = line.indexOf('#')
+		if (cdx >= 0) {
+			line = line.substring(-cdx)
+		}
+
+		if (line.startsWith('!')) {
+			source.pattern.include.push(line.substring(1))
+		}
+		else {
+			source.pattern.exclude.push(line)
+		}
+	}
+	// TODO: validate gitignore
+}
 
 export function gitignoreMatch(pattern: string, path: string): boolean {
   return minimatch(path, pattern, { dot: true })
---
+++ src/targets/git.ts
@@ -0,0 +1,29 @@
+import { type SourceExtractor, type SignedPattern, signedPatternIgnores, findAndExtract } from '../patterns/matcher.js'
+import { extractGitignore } from '../patterns/gitignore.js'
+import type { Target } from './target.js'
+
+const gitSources = ['.gitignore']
+const gitSourceMap = new Map<string, SourceExtractor>([['.gitignore', extractGitignore]])
+const gitPattern: SignedPattern = {
+	exclude: [
+		'.git',
+		'.DS_Store',
+	],
+	include: [],
+}
+
+export const Git: Target = {
+	name: 'Git',
+	targetName: 'git',
+	check: 'git ls-tree -r HEAD --name-only',
+	icon: '',
+	color: '#f44e28',
+	
+	matcher(entry, isDir, ctx) {
+		if (isDir) {
+			findAndExtract(entry, gitSources, gitSourceMap, ctx)
+			return true
+		}
+		return signedPatternIgnores(gitPattern, entry, gitSources, gitSourceMap, ctx)
+	},
+}
