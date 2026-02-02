bb7588e5a6856704e0b6e8a3c06ee1fa4602dae5
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2026-02-02 23:20:13 +0100

    fs

--- internal/mod.go
+++ internal/mod.go
@@ -3,6 +3,8 @@
 package internal
 
 import (
+	"errors"
+	"io/fs"
 	"syscall/js"
 
 	"github.com/Mopsgamer/view-ignored/internal/targets"
@@ -26,6 +28,7 @@ func scan(this js.Value, args []js.Value) any {
 	ctx := Scan(ScanOptions{
 		Target: target,
 		Cwd:    new(options.Get("cwd").String()),
+		FS:     jsFs{},
 	})
 
 	return ctx
@@ -34,4 +37,101 @@ func scan(this js.Value, args []js.Value) any {
 func main() {
 	js.Global().Set("scan", js.FuncOf(scan))
 	select {}
+}
+
+type jsFs struct {
+	fs js.Value // Reference to the Node.js 'fs' module
+}
+
+var _ fs.FS = (*jsFs)(nil)
+
+func (sys jsFs) Open(name string) (fs.File, error) {
+	// This method should implement JS-compatible
+	// file opener, knowing that sys provides a
+	// "NodeJS fs module"-compatible object: { promises }
+	// where promises provides:
+	// - opendir(path) => Promise<Dir>,
+	// - readFile(path, ?{encoding}): Promise<Buffer<ArrayBuffer>>
+	// - stat(path): Promise<Stat>
+	return nil, errors.New("not implemented")
+}
+
+func await(promise js.Value) (js.Value, error) {
+	resCh := make(chan js.Value)
+	errCh := make(chan error)
+
+	success := js.FuncOf(func(this js.Value, args []js.Value) any {
+		resCh <- args[0]
+		return nil
+	})
+	defer success.Release()
+
+	failure := js.FuncOf(func(this js.Value, args []js.Value) any {
+		errCh <- errors.New(args[0].Get("message").String())
+		return nil
+	})
+	defer failure.Release()
+
+	promise.Call("then", success).Call("catch", failure)
+
+	select {
+	case res := <-resCh:
+		return res, nil
+	case err := <-errCh:
+		return js.Undefined(), err
+	}
+}
+
+var _ fs.File = (*jsFile)(nil)
+
+type jsFile struct{}
+
+func (f *jsFile) Stat() (fs.FileInfo, error) { return nil, errors.New("not implemented") }
+func (f *jsFile) Close() error               { return errors.New("not implemented") }
+func (f *jsFile) Read(p []byte) (int, error) {
+	return 0, errors.New("not implemented")
+}
+
+var _ fs.ReadDirFile = (*jsDir)(nil)
+
+type jsDir struct{}
+
+func (d *jsDir) Stat() (fs.FileInfo, error) {
+	// export interface Stat {
+	//     isFile(): boolean;
+	//     isDirectory(): boolean;
+	//     isBlockDevice(): boolean;
+	//     isCharacterDevice(): boolean;
+	//     isSymbolicLink(): boolean;
+	//     isFIFO(): boolean;
+	//     isSocket(): boolean;
+	//     dev: number;
+	//     ino: number;
+	//     mode: number;
+	//     nlink: number;
+	//     uid: number;
+	//     gid: number;
+	//     rdev: number;
+	//     size: number;
+	//     blksize: number;
+	//     blocks: number;
+	//     atimeMs: number;
+	//     mtimeMs: number;
+	//     ctimeMs: number;
+	//     birthtimeMs: number;
+	//     atime: Date;
+	//     mtime: Date;
+	//     ctime: Date;
+	//     birthtime: Date;
+	// }
+	return nil, errors.New("not implemented")
+}
+func (d *jsDir) Read(p []byte) (int, error) {
+	return 0, &fs.PathError{Op: "read", Path: "", Err: errors.New("is a directory")}
+}
+func (d *jsDir) Close() error {
+	return errors.New("not implemented")
+}
+func (d *jsDir) ReadDir(n int) ([]fs.DirEntry, error) {
+	return nil, errors.New("not implemented")
 }
