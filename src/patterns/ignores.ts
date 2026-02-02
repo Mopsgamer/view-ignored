6352116a6f3a0e0772a3b93a749a040c22e494ba
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2026-02-02 21:11:55 +0100

    move ignores

---
+++ src/patterns/ignores.ts
@@ -0,0 +1,16 @@
+import type { FsAdapter } from "../types.js";
+import type { MatcherContext } from "../patterns/matcherContext.js";
+import type { SignedPatternMatch } from "../patterns/signedPattern.js";
+
+/**
+ * Checks whether a given entry path should be ignored based on its patterns.
+ * @see {@link sourcesBackwards}
+ * @see {@link signedPatternIgnores}
+ * @see {@link https://github.com/Mopsgamer/view-ignored/tree/main/src/targets} for usage examples.
+ */
+export type Ignores = (
+	fs: FsAdapter,
+	cwd: string,
+	entry: string,
+	ctx: MatcherContext,
+) => Promise<SignedPatternMatch>
