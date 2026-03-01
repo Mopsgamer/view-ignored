e828affd8f5142e3384c0b637a130d2788805cec
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2026-03-01 21:34:03 +0100

    perf: opendir

--- src/opendir.ts
+++ src/opendir.ts
@@ -8,20 +8,27 @@ export async function opendir(
 	cb: (entry: Dirent, from: string) => Promise<0 | 1 | 2>,
 ): Promise<void | 2> {
 	const dir = await fs.promises.opendir(path)
+	const tasks: Promise<void | 2>[] = []
+
 	for await (const entry of dir) {
 		const from = path + "/" + entry.name
-		const r = await cb(entry, from)
-		if (r === 2) {
-			return 2
-		}
-		if (r === 1) {
-			continue
-		}
-		if (entry.isDirectory()) {
-			const r = await opendir(fs, from, cb)
-			if (r === 2) {
-				return 2
+
+		const task = (async (): Promise<void | 2> => {
+			const r = await cb(entry, from)
+			if (r === 2) return 2
+			if (r === 1) return
+
+			if (entry.isDirectory()) {
+				return await opendir(fs, from, cb)
 			}
-		}
+		})()
+
+		tasks.push(task)
+	}
+
+	const results = await Promise.all(tasks)
+
+	if (results.includes(2)) {
+		return 2
 	}
 }
