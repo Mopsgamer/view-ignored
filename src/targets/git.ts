9d5b9fab47576a53df8ec590b11d6230faccbec5
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2026-05-25 17:25:07 +0200

    ini

--- src/targets/git.ts
+++ src/targets/git.ts
@@ -1,4 +1,5 @@
 import type { Target } from "./target.js"
+import * as ini from "ini"
 
 import {
 	type Extractor,
@@ -19,12 +20,19 @@ const extractors: Extractor[] = [
 	},
 ]
 
+const fromConfigs = {
+	compiled: null,
+	excludes: true,
+	pattern: [],
+}
+
 const internal: Rule[] = [
 	ruleCompile({
 		compiled: null,
 		excludes: true,
 		pattern: [".git", ".DS_Store"],
 	}),
+	fromConfigs,
 ]
 
 /**
@@ -32,8 +40,60 @@ const internal: Rule[] = [
  */
 export const Git: Target = <Target>{
 	extractors,
-	// TODO: Git should read configs
 	ignores: ruleTest,
+	init() {
+		// do not delete comments in this function
+		// TODO: Git should read configs
+		// put it into fromConfigs variable
+		// oxlint-disable-next-line no-unused-expressions
+		fromConfigs
+		// use already imported library ini
+		ini.parse("")
+		/* [core] excludesFile
+		Specifies the pathname to the file that contains
+		patterns to describe paths that are not meant to be tracked,
+		in addition to .gitignore (per-directory) and .git/info/exclude.
+		Defaults to $XDG_CONFIG_HOME/git/ignore.
+		If $XDG_CONFIG_HOME is either not set or empty,
+		$HOME/.config/git/ignore is used instead.
+		*/
+		/* [include] path ...
+		 * [includeIf "condition"] path ...
+		[include]
+			path = /path/to/foo.inc ; include by absolute path
+			path = foo.inc ; find "foo.inc" relative to the current file
+			path = ~/foo.inc ; find "foo.inc" in your `$HOME` directory
+
+		; include if $GIT_DIR is /path/to/foo/.git
+		[includeIf "gitdir:/path/to/foo/.git"]
+			path = /path/to/foo.inc
+
+		; include for all repositories inside /path/to/group
+		[includeIf "gitdir:/path/to/group/"]
+			path = /path/to/foo.inc
+
+		; include for all repositories inside $HOME/to/group
+		[includeIf "gitdir:~/to/group/"]
+			path = /path/to/foo.inc
+
+		; relative paths are always relative to the including
+		; file (if the condition is true); their location is not
+		; affected by the condition
+		[includeIf "gitdir:/path/to/group/"]
+			path = foo.inc
+
+		; include only if we are in a worktree where foo-branch is
+		; currently checked out
+		[includeIf "onbranch:foo-branch"]
+			path = foo.inc
+
+		; include only if a remote with the given URL exists (note
+		; that such a URL may be provided later in a file or in a
+		; file read after this file is read, as seen in this example)
+		[includeIf "hasconfig:remote.*.url:https://example.com/**"]
+			path = foo.inc
+		*/
+	},
 	internalRules: internal,
 	root: "/",
 }
