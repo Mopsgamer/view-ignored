f36ce0be4ddfb3f55075b95e27a0bf18ad2be1bb
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2026-08-16 16:53:58 +0200

    fix(git): don't ign .DS_Store

--- src/targets/git.ts
+++ src/targets/git.ts
@@ -34,7 +34,7 @@ export function makeGit(): Target {
 	cachedGitRule ||= ruleCompile({
 		compiled: null,
 		excludes: true,
-		list: [".git", ".DS_Store"],
+		list: [".git"],
 	})
 
 	const internal: InternalRules = {
